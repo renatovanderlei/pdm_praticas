ac959f661cea7f2bfb93daff8293585566228ca9
Renato Vanderlei <nato_vanderlei@hotmail.com>
2024-12-16 13:46:19 -0300

    Prática 02 – Preparação

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.weatherapp.ui.HomePage
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,31 +21,6 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     HomePage(modifier = Modifier.padding(innerPadding))
                 }
-            }
-        }
-    }
-}
-
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as? Activity // Inicialize aqui
-
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Text(text = "Bem-vindo ao WeatherApp!")
-
-            Button(onClick = {
-                activity?.finish() // Encerra a atividade e volta à tela de login
-            }) {
-                Text(text = "Sair")
             }
         }
     }
