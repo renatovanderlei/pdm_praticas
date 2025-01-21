d1e16b32acdc89141fa5098d5c37a8afd4392638
Renato Vanderlei <nato_vanderlei@hotmail.com>
2025-01-21 00:23:23 -0300

    Prática 04 – Parte 4

--- app/src/main/java/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/weatherapp/ui/MapPage.kt
@@ -1,14 +1,23 @@
 package com.weatherapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.weatherapp.model.MainViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
@@ -25,10 +34,21 @@ fun MapPage(
     val recife = LatLng(-8.05, -34.9)
     val caruaru = LatLng(-8.27, -35.98)
     val joaopessoa = LatLng(-7.12, -34.84)
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
 
     GoogleMap(
         modifier = modifier.fillMaxSize(),
         cameraPositionState = camPosState,
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true),
         onMapClick = { viewModel.add("Nova cidade", location = it) }
     ) {
         // Adicionar marcadores para as localizações predefinidas
